8aa81e8639a76f623ff220944f4abe0753ae786a
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-03-31 16:19:25 +0430

    add: script to generate emoji valid filenames json.
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>

---
+++ script/filename_generator.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "negarmoji"
+require "json"
+
+file_names = Emoji.all.map(&:image_filename).flatten
+
+File.open(ARGV[0], "w") do |file|
+  file.write(JSON.pretty_generate(file_names))
+end
