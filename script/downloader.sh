d6f2fdf624c6d750ea6013d9d312827b21562735
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2021-07-20 14:08:59 +0430

    modify: use wget instead of curl for downloading

--- script/downloader.sh
+++ script/downloader.sh
@@ -4,12 +4,26 @@
 VENDORDIR="${PWD%/*}"/vendor
 TEMPDIR="temp"
 
+# download links.
+OPENMOJI_JSON_LINK="https://raw.githubusercontent.com/hfg-gmuend/openmoji/master/data/openmoji.json"
+GEMOJI_JSON_LINK="https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json"
+GEMOJI_TEST_JSON_LINK="https://raw.githubusercontent.com/github/gemoji/master/vendor/unicode-emoji-test.txt"
+
+# setup paths.
+OPENMOJI_JSON="${TEMPDIR}/openmoji.json"
+GEMOJI_JSON="${TEMPDIR}/gemoji.json"
+GEMOJI_TEST_JSON="${VENDORDIR}/unicode-emoji-test.txt"
+
 # create temporary directory for files.
-mkdir -p $TEMPDIR
+mkdir --parents "${TEMPDIR}"
 
 # download gemoji and openmoji json files.
-curl -L https://raw.githubusercontent.com/hfg-gmuend/openmoji/master/data/openmoji.json --output $TEMPDIR/openmoji.json
-curl -L https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json --output $TEMPDIR/gemoji.json
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 \
+--tries 0 --no-dns-cache --output-document "${OPENMOJI_JSON}" "${OPENMOJI_JSON_LINK}"
+
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 \
+--tries 0 --no-dns-cache --output-document "${GEMOJI_JSON}" "${GEMOJI_JSON_LINK}"
 
 # download latest unicode emoji test from gemoji.
-curl -L https://raw.githubusercontent.com/github/gemoji/master/vendor/unicode-emoji-test.txt --output "$VENDORDIR"/unicode-emoji-test.txt
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 \
+--tries 0 --no-dns-cache --output-document "${GEMOJI_TEST_JSON}" "${GEMOJI_TEST_JSON_LINK}"
