0d49dfda3f88eb00ab47ba4e34594d0d78d83659
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2021-07-21 19:57:15 +0430

    modify: uncomment set -e to exit during ci if a test fails

--- script/test.sh
+++ script/test.sh
@@ -6,7 +6,7 @@
 # set flag for shell execution.
 # -e  Exit immediately if a command exits with a non-zero status.
 # -x  Print commands and their arguments as they are executed.
-# set -e
+set -e
 
 # files for testing.
 NEGAR_RB="../lib/negarmoji.rb"
