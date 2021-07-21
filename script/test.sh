33683cafdd70f56e879f0e698c973c7179fbd20d
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2021-07-21 19:28:35 +0430

    modify: explicitly define files for tests

--- script/test.sh
+++ script/test.sh
@@ -6,7 +6,13 @@
 # set flag for shell execution.
 # -e  Exit immediately if a command exits with a non-zero status.
 # -x  Print commands and their arguments as they are executed.
-set -ex
+# set -e
 
-./test_style.sh
-./test_module.sh
+# files for testing.
+NEGAR_RB="../lib/negarmoji.rb"
+CHAR_RB="../lib/negarmoji/character.rb"
+EMOJI_RB="../lib/negarmoji/emoji.rb"
+VERSION_RB="../lib/negarmoji/version.rb"
+
+./test_style.sh "${NEGAR_RB}" "${CHAR_RB}" "${EMOJI_RB}" "${VERSION_RB}"
+./test_module.sh "${NEGAR_RB}" "${CHAR_RB}" "${EMOJI_RB}" "${VERSION_RB}"
